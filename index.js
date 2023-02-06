0910aa1dd39a30902e7f5f0a05559f44d7761279
Sheravil <Sheravil>
2023-02-06 17:16:25 +0300

    desktop fontsize updated

--- index.js
+++ index.js
@@ -36,9 +36,9 @@ carouselDesktop.innerHTML = `<div> <ol class="carousel-indicators">
 <div class="carousel-inner">
 <div class="carousel-item active">
   <img  src="/img/takim1.jpg" class="d-block w-100" alt="...">
-  <div class="carousel-caption" style="height: 35%;">
-    <h2>İmtan Kaporta</h2>
-    <p>Boyasız Göçük Onarım Merkezi</p>
+  <div class="carousel-caption" style="height: 38%;">
+    <h2 style="font-size:5rem">İmtan Kaporta</h2>
+    <h2>Boyasız Göçük Onarım Merkezi</h2>
     <br>
   </div>
    
